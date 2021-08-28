ba1a41616c4f4b494c38d2c6da9194404002b379
Jonas Holbech <jh@jonasholbech.dk>
2021-08-28 10:25:29 +0200

    solve exercise

--- superheroes.js
+++ superheroes.js
@@ -407,9 +407,23 @@ const superheroes = [
   },
 ];
 
-alert(
-  "1. Checking if JS file is linked up correctly. \n2. Always open the inspector/console when woorking with the JS \n3. Write your code in the end of the js-file \n4. Don't use alert boxes for UI... \n5. Delete this message or comment it out"
-);
 console.table(superheroes);
 
 /* write your code here */
+superheroes.forEach(showHero);
+
+function showHero(hero) {
+  console.log(hero);
+  const template = document.querySelector("template").content;
+  const clone = template.cloneNode(true);
+  clone.querySelector(".alias").textContent = hero.alias;
+  clone.querySelector(".birthname").textContent = hero.birthname;
+  clone.querySelector("img").src = hero.img;
+  clone.querySelector("img").alt = hero.alias;
+  if (hero.cape) {
+    //nothing
+  } else {
+    clone.querySelector("p").remove();
+  }
+  document.querySelector("main").appendChild(clone);
+}
